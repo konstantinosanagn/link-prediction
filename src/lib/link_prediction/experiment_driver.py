9f3375642c6b81bf7bbe93efae3596b4688dc4b2
davidq2010 <36721469+davidq2010@users.noreply.github.com>
2024-01-23 08:59:15 -0800

    User/davidqin/prompt experiment initial (#2)
    * Prompting initial commit
    
    * Working decent zero-shot prompt example
    
    * Updated prompt suffix for consistent answer format.
    
    * Fix bug in deserialization, light testing
    
    * Update initial experiments
    
    * Consolidate experiment scripts
    
    * Fix some pkg layout
    
    * Fix small bugs
    
    * Format generated results correctly
    
    * Print stmnt
    
    * Attempted batching fix
    
    * Debugging
    
    * Initial working experiment
    
    * Initial checkin configurable experiments
    
    * Update README and update default args
    
    * Fix README main reference
    
    * Restructure packages
    
    * Temporarily remove lib from gitignore
    
    * Proper linking
    
    * Update config
    
    * Experiment config
    
    * Generalizable dataset deserializer
    
    * Update refs
    
    * Fix bugs
    
    * Fix broken import
    
    * Fix class references in config
    
    * typo
    
    * seed
    
    * Named args
    
    * Incorrect use of set
    
    * print stuff
    
    * Printing stuff for debugging
    
    * Print more for debugging
    
    * Debugging
    
    * Moved main to for more correct directory structure. More search patterns for assistant response.
    
    * absolute import
    
    * Debugging print statements
    
    * Fix print statements
    
    * Don't throw on failure to parse assistant response
    
    * More debugging
    
    * Fix sklearn split error
    
    * Fix sklearn split error
    
    * Fix ref to validation data when None
    
    * Attempt to debug when we have split data
    
    * Don't explicitly set both train_size and test_size for train_test_split

---
+++ src/lib/link_prediction/experiment_driver.py
@@ -0,0 +1,88 @@
+"""
+Module containing function to run a prompting experiment.
+"""
+
+from typing import List, Optional
+import pandas as pd
+import numpy as np
+
+from llama import Llama, Dialog
+
+from src.lib.dataset_loaders import SplitData
+from .dialog_formatters import get_dialogs, Example
+from .assistant_response_parsers import BaseResponseParser
+
+def run_experiment(
+    generator: Llama,
+    response_parser: BaseResponseParser,
+    data: SplitData,
+    system_prompt: str,
+    user_prompt_format: str,
+    temperature: float,
+    top_p: float,
+    max_batch_size: int,
+    max_gen_len: Optional[int],
+    run_on_validation = False,
+    run_on_test = False
+):
+    """
+    Runs prompting experiments.
+
+    Args:
+        generator (Llama): Generates Llama assistant responses.
+        response_parser (BaseResponseParser): Object to parse assistant responses.
+        data (SplitData): Typed dict mapping data type (train, validation, test) to a dataset.
+        system_prompt (str): A system prompt to provide the model with context.
+        user_prompt_format (str): A format string for the user prompt.
+        temperature (float, optional): The temperature value for controlling randomness in generation.
+            Defaults to 0.6.
+        top_p (float, optional): The top-p sampling parameter for controlling diversity in generation.
+            Defaults to 0.9.
+        max_batch_size (int, optional): The maximum batch size for generating sequences. Defaults to 8.
+        max_gen_len (int, optional): The maximum length of generated sequences. If None, it will be
+            set to the model's max sequence length. Defaults to None.
+        run_on_validation (bool): Whether to also generate responses for prompts from the validation set.
+            Defaults to False.
+        run_on_test (bool): Whether to also generate responses for prompts from the test set.
+            Defaults to False.
+    """
+    # TODO: Validate args?
+    splits_to_use = [data['train']]
+    if run_on_validation and data['validation']:
+        splits_to_use.append(data['validation'])
+    if run_on_test and data['test']:
+        splits_to_use.append(data['test'])
+    for split in splits_to_use:
+        # TODO: Log to see the token input to the model.
+        user_prompts = [user_prompt_format.format(sample.text,
+                                                  response_parser.answer_format.format(response_parser.answer_token))
+                        for sample in split]
+        examples = [Example(
+            user_prompt=user_prompt_format.format(example.text, response_parser.answer_format.format(response_parser.answer_token)),
+            assistant_response=response_parser.answer_format.format(example.type))
+                    for example in data['examples']]
+        expected_results = [sample.type.lower() for sample in split]
+        dialogs: List[Dialog] = get_dialogs(system_prompt, user_prompts, examples, True)
+        results = []
+        # Have to submit prompts in batches
+        for i in range(0, len(dialogs), max_batch_size):
+            dialogs_batch = dialogs[i:i+max_batch_size] if i+max_batch_size < len(dialogs) else dialogs[i:]
+            print(f"Dialogs: {dialogs_batch}")
+            batch_results = generator.chat_completion(
+                dialogs_batch,
+                max_gen_len=max_gen_len,
+                temperature=temperature,
+                top_p=top_p
+            )
+            print(f"Generated results: {[result['generation']['content'] for result in batch_results]}")
+            parsed_results = [response_parser.get_parsed_response(result['generation']['content']) for result in batch_results]
+            print(f"Parsed results: {parsed_results}")
+            results += parsed_results
+        print("=================================================================")
+
+        # Save results
+        # TODO: Record the success rate of each proposition type
+        df = pd.DataFrame(data={"Id": [t.id for t in split], "Actual": results, "Expected": expected_results})
+        df["AreEqual"] = np.where(df["Actual"] == df["Expected"], 1, 0)
+        accuracy = sum(df["AreEqual"]) / len(df["AreEqual"])
+        print(f"Accuracy: {accuracy}")
