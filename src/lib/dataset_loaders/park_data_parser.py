ead8d327e50d9f04a1dbb5d84eb095b49355d5df
konstantinosanagn <konstantinos.anagnostopoulos@richmond.edu>
2024-05-20 12:00:29 -0400

    - changes in: enhancedDatasetLoader, SupportResponseParser, new_experiment_driver, new_main

--- src/lib/dataset_loaders/park_data_parser.py
+++ src/lib/dataset_loaders/park_data_parser.py
@@ -8,7 +8,6 @@ import random
 from typing import List
 from dataclasses import dataclass
 
-# from src.lib.dataset_loaders.amazon_review_dataset_loader import SplitData
 
 @dataclass
 class Proposition:
@@ -29,8 +28,17 @@ class Comment:
     propositions: List[Proposition]
     id: str
 
-def deserialize_comments_jsonlist(filepath: str)\
-        -> List[Comment]:
+def deserialize(obj) -> Comment:
+    propositions_raw = obj['propositions']
+    propositions = [Proposition(id=prop['id'],
+                                type=prop['type'],
+                                text=prop['text'],
+                                reasons=prop.get('reasons'),
+                                evidence=prop.get('evidence'))
+                    for prop in propositions_raw]
+    return Comment(id=obj['id'], propositions=propositions)
+
+def deserialize_comments_jsonlist(filepath: str) -> List[Comment]:
     """
     Deserializes file containing a list of JSON Amazon Review objects.
     Expects each review to be on a new line.
@@ -40,28 +48,15 @@ def deserialize_comments_jsonlist(filepath: str)\
     Returns:
         List[AmazonReview]: List of deserialized Amazon Reviews.
     """
-    def deserialize(obj):
-        propositions_raw = obj['propositions']
-        propositions = [Proposition(id=prop['id'],
-                                   type=prop['type'],
-                                   text=prop['text'],
-                                   reasons=prop['reasons'],
-                                   evidence=prop['evidence'])
-                        for prop in propositions_raw]
-        return Comment(propositions=propositions, id=obj['id'])
+    
     comments = []
     with open(filepath, 'r', encoding='utf-8') as f:
         for line in f:
-            comments.append(deserialize(json.loads(line)))
+            try:
+                comments.append(deserialize(json.loads(line)))
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
     return comments
-
-# def serialize_comments_jsonlists(directory_path: str, comments: List[Comment], train: float, validation: float, test: float):
-#     """
-#     Serializes comments to traini, validation, and test under directory_path.
-#     """
-#     shuffled_comments = random.sample(comments, len(comments))
-#     with open(split_filepath, 'w', encoding='utf-8') as f:
-        
 
 
 if __name__ == '__main__':
